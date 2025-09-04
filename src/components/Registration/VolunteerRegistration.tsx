22ff2cebae9f13f7c7ee2efcdf280202c32ae48b
Suresh Dokka <112100121+Suresh-d-7@users.noreply.github.com>
2025-09-04 08:04:23 +0530

    Suresh - API Integration added.

--- src/components/Registration/VolunteerRegistration.tsx
+++ src/components/Registration/VolunteerRegistration.tsx
@@ -1,21 +1,40 @@
 import { Col, Container, Row } from "react-bootstrap";
 import TextField from "../UI/TextField";
-import { z } from "zod";
+import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import SelectField from "../UI/SelectField";
 import DateField from "../UI/DateField";
+import useVMSMutation from "../../Hooks/useVMSMutation";
+import { ToastContainer } from "react-toastify";
+
+interface RegistrationFormValues {
+  firstname: string;
+  lastname: string;
+  dateofregistration: string;
+  dateofbirth: string;
+  volunteerinitials: string;
+  gender: string;
+  relationtype: string;
+  fathername: string;
+  age: string;
+  diet: string;
+  race: string;
+  weight: string;
+  height: string;
+  bmi: string;
+}
 
 export const VolunteerRegistration = () => {
-  const RegistrationInitialValues = {
+  const RegistrationInitialValues: RegistrationFormValues = {
     firstname: "",
     lastname: "",
     dateofregistration: "",
+    dateofbirth: "",
     volunteerinitials: "",
     gender: "",
     relationtype: "",
     fathername: "",
-    dateofbirth: "",
     age: "",
     diet: "",
     race: "",
@@ -23,12 +42,68 @@ export const VolunteerRegistration = () => {
     height: "",
     bmi: "",
   };
-  const RegistrationSchema = z.object({});
-  const RegistrationForm = useForm<any>({
+
+  const RegistrationSchema = z.object({
+    firstname: z.string({ required_error: "First Name is required" }),
+    lastname: z.string(),
+    dateofregistration: z.string(),
+    volunteerinitials: z.string(),
+    dateofbirth: z.string(),
+    gender: z.string(),
+    relationtype: z.string(),
+    fathername: z.string(),
+    age: z.string(),
+    diet: z.string(),
+    race: z.string(),
+    weight: z.string(),
+    height: z.string(),
+    bmi: z.string(),
+  });
+
+  const RegistrationForm = useForm<RegistrationFormValues>({
     mode: "onChange",
     defaultValues: RegistrationInitialValues,
     resolver: zodResolver(RegistrationSchema),
   });
+
+  const formSubmit = () => {
+    const valuesform = RegistrationForm.getValues();
+    console.log(valuesform, "valuesform");
+
+    const postParams = {
+      registrationNo : 1,
+      dateofRegistration : valuesform.dateofregistration,
+      volunteerIntials : valuesform.volunteerinitials,
+      firstName : valuesform.firstname,
+      surName : valuesform.lastname,
+      gender : valuesform.gender,
+      fatherorHusbandName : valuesform.fathername,
+      dateofBirth : valuesform.dateofbirth,
+      age : valuesform.age,
+      diet : valuesform.diet,
+      race : valuesform.race,
+      weight : valuesform.weight,
+      bmi : valuesform.bmi,
+      // identificationMarks : "NA",
+      // addfingerprints : "NA",
+      // takePhoto : "NA",
+      // files : "NA",
+      height : valuesform.height,
+    }
+    insertRegistration.mutateAsync({postParams: postParams})
+  }
+
+  const insertRegistration = useVMSMutation({
+    endPointUrl: "registration/userRegistrationStep1",
+    onSuccess: (response : any) => {
+      if(response.status === 201){
+        const message = response.data.message;
+        alert(message);
+      }
+      console.log("Registration Success");
+    },
+    key: "registration",
+  })
 
   return (
     <>
@@ -64,6 +139,22 @@ export const VolunteerRegistration = () => {
             <DateField
               name={"dateofregistration"}
               label={"Date of Registration"}
+              control={RegistrationForm.control}
+              required={true}
+            />
+          </Col>
+          <Col xl={4} lg={4} md={4} sm={4}>
+            <DateField
+              name={"dateofbirth"}
+              label={"Date of Birth"}
+              control={RegistrationForm.control}
+              required={true}
+            />
+          </Col>
+          <Col xl={4} lg={4} md={4} sm={4}>
+            <TextField
+              name={"race"}
+              label={"Race"}
               control={RegistrationForm.control}
               required={true}
             />
@@ -123,7 +214,13 @@ export const VolunteerRegistration = () => {
               required={true}
             />
           </Col>
+          <div>
+            <button style={{ borderRadius: "5px" }} onClick={RegistrationForm.handleSubmit(formSubmit)}>
+              Submit
+            </button>
+          </div>
         </Row>
+        <ToastContainer />
       </Container>
     </>
   );
